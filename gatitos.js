aa454bf61873fbbaa4bcd7153b394d49c650e1a1
ruben-rico <zoazdark@gmail.com>
2022-07-26 12:17:30 -0500

    practica gatitos en git

---
+++ gatitos.js
@@ -0,0 +1,29 @@
+var URL="https://api.thecatapi.com/v1/images/search";
+var imagen = document.querySelector("img");
+var boton = document.querySelector("button")
+
+
+
+function peticionGatito ( ){
+    fetch(URL)
+.then(function(response){
+    //esta funcion se ejecuta si salio bien la peticion 
+    //console.log(response); 
+    return response.json();  
+})
+.then(function(data){
+    console.log(data[0].url);
+    imagen.src=data[0].url
+})
+.catch(function(error){
+    console.log(error);
+});
+}
+
+document.addEventListener("DOMContentLoaded",function(){
+    peticionGatito();
+})
+
+boton.addEventListener("click",function(){
+    peticionGatito()
+})
